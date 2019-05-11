8371d1b390d94123a2ffa32f98460be2f77ee98d
braywolf11 <brayfer1893@gmail.com>
2019-05-10 20:02:08 -0500

    taller en clase

--- backup_db/tienda.sql
+++ backup_db/tienda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-05-2019 a las 04:58:43
+-- Tiempo de generación: 10-05-2019 a las 05:00:04
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
@@ -48,6 +48,32 @@ INSERT INTO `productos` (`id`, `codigo_pro`, `nombre_pro`, `cantidad`, `ruta_ima
 (6, '123456', 'sapote', 60, NULL, NULL),
 (7, '123456', 'pera', 60, NULL, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(30) COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(30) COLLATE utf8_bin NOT NULL,
+  `sex` varchar(1) COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(50) COLLATE utf8_bin NOT NULL,
+  `photo` varchar(50) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `firstname`, `lastname`, `sex`, `email`, `password`, `photo`) VALUES
+(1, 'Brayan', 'Rivera', '', 'brayfer1893@gmail.com', '23132131', NULL),
+(2, 'angela', 'solarte', '', 'solarte@gmail.com', '132156456', NULL),
+(3, 'pedro', 'rivas', '', 'rivas1415@gmail.com', '$2y$10$q9HYPHj2SGTcQsJOKIN9T.TYFChPWoZD39.gCbY6c5.', 'images/avatar_default.png'),
+(4, 'Brayan', 'Rivera', 'M', 'brayfer18933333@gmail.com', '$2y$10$kok2ABaLdocbeak8q2fnS.FMvHAyZzVrK1yLRbeosVl', 'images/avatar_default.png');
+
 --
 -- Índices para tablas volcadas
 --
@@ -59,6 +85,12 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -67,6 +99,12 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `productos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
