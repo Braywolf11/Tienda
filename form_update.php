a4f8af7b7ea5dfc4655f15293f931faf9a65d387
braywolf11 <brayfer1893@gmail.com>
2019-05-17 20:03:40 -0500

    actualizar

--- form_update.php
+++ form_update.php
@@ -1,9 +1,28 @@
 <?php
      $id=$_GET['id'];
-     $cod=$_GET['cod'];
-     $nom=$_GET['nom'];
-     $cant=$_GET['can'];
+     //$cod=$_GET['cod'];
+     //$nom=$_GET['nom'];
+     //$cant=$_GET['can'];
+     include "database.php";
+     $n=1;
+       $sql="SELECT * FROM productos where id = $id limit 1";
+       $result=$conn->query($sql);
 
+       if($result->num_rows > 0){
+           while ($row = $result->fetch_assoc())
+             {
+                $codprod = $row['codigo_pro'];
+                $nomprod = $row['nombre_pro'];
+                $cantprod = $row['cantidad'];
+               echo "<tr>";
+                //echo "<td>".$row['codigo_pro']."</td>";
+                //echo "<td>".$row['nombre_pro']."</td>";
+                //echo "<td>".$row['cantidad']."</td>";
+                //echo "<td><a href = 'form_update.php?id=".$row['id']."& cod=".$row['codigo_pro']."& nom=".$row['nombre_pro']."& can=".$row['cantidad']."'><img src = 'icons/edit.png' width='30'></a></td>";
+                //echo "<td><a href = 'delete.php?id=".$row['id']."& cod=".$row['codigo_pro']."'><img src = 'icons/delete.png' width='30'></a></td>";
+               echo "<tr>";
+           }
+        }
 
     
 
@@ -31,16 +50,16 @@
                         <tr>
                             <td bgcolor= "#183E26"><b>Codigo de Producto:</b><font color="red">*</font></td>
 
-                            <td bgcolor= "#fbfbfb"><b><input type="text" name="cod_pro" placeholder="CODIGO" minlength="1" maxlength="30" pattern="[A-Za-z]{4-16}" value=<?php echo $cod;?>  required></b></td>
+                            <td bgcolor= "#fbfbfb"><b><input type="text" name="cod_pro" placeholder="CODIGO" minlength="1" maxlength="30" pattern="[A-Za-z]{4-16}" value=<?php echo $codprod;?>  required></b></td>
                         </tr>
                         
                         <tr>
                             <td bgcolor= "#183E26"><b>Nombre Producto:</b><font color="red">*</font></td>
-                            <td bgcolor= "#fbfbfb"><b><input type="text" name="nom_prod" placeholder="NOMBRE" minlength="1" maxlength="30" pattern="[A-Za-z]{4-16}" value=<?php echo $nom;?> required></b></td>
+                            <td bgcolor= "#fbfbfb"><b><input type="text" name="nom_prod" placeholder="NOMBRE" minlength="1" maxlength="30" pattern="[A-Za-z]{4-16}" value=<?php echo $nomprod;?> required></b></td>
                         </tr>
                         <tr>
                             <td bgcolor= "#183E26"><b>Catidad:</b><font color="red">*</font></td>
-                            <td bgcolor= "#fbfbfb"><b><input type="text" name="Cantidad" placeholder="CANTIDAD" minlength="1" maxlength="30" pattern="[A-Za-z]{4-16}" value=<?php echo $cant;?> required></b></td>
+                            <td bgcolor= "#fbfbfb"><b><input type="text" name="Cantidad" placeholder="CANTIDAD" minlength="1" maxlength="30" pattern="[A-Za-z]{4-16}" value=<?php echo $cantprod;?> required></b></td>
                         </tr>
                       
                         
